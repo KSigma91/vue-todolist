3b63536330abe0c9b5504115f70226ccfd91b911
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-27 10:11:28 +0200

    milestone 3

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-/*
-Descrizione:
-Rifare l’esercizio della to do list.
-Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
- - text, una stringa che indica il testo del todo
-- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-
-MILESTONE 1
-Stampare all’interno di una lista, un item per ogni todo.
-Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-
-MILESTONE 2
-Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
-
-MILESTONE 3
-Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
-*/
-
 var app = new Vue({
     el : '#container',
     data : {
@@ -52,9 +34,13 @@ var app = new Vue({
             this.listArray.splice(index, 1);
         },
         addItem() {
-            if(this.listArray.length !== "") {
-                this.listArray.push(this.newItem);
-                this.newItem = "";
+            var newitemArray = {
+                text : this.newItem,
+                done : false,
+            }
+            if(newitemArray !== "") {
+                this.listArray.push(newitemArray);
+                newitemArray = "";
             }
         }
     }
